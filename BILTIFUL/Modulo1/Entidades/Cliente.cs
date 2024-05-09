ac0ed292c4c7b6092de16d30e1ebbe1632900bb6
Edenilson <edenilsonju10@gmail.com>
2024-05-09 18:43:24 -0300

    Implementacao do construtor da classe cliente com string data

--- BILTIFUL/Modulo1/Entidades/Cliente.cs
+++ BILTIFUL/Modulo1/Entidades/Cliente.cs
@@ -8,5 +8,59 @@ namespace BILTIFUL.Modulo1
 {
     internal class Cliente
     {
+        public string Cpf { get; set; }                 //11 (0-10)
+        public string Nome { get; set; }                //50 (11-60)
+        public DateOnly DataNascimento { get; set; }    //8  (61-68)
+        public char Sexo { get; set; }                  //1  (69-69)
+        public DateOnly UltimaCompra { get; set; }      //8  (70-77)
+        public DateOnly DataCadastro { get; set; }      //8  (78-85)
+        public char Situacao { get; set; }              //1  (86-86)
+
+        public Cliente(string cpf, string nome, DateOnly dataNascimento, char sexo)
+        {
+            Cpf = cpf;
+            Nome = nome;
+            DataNascimento = dataNascimento;
+            Sexo = sexo;
+            DataCadastro = DateOnly.FromDateTime(DateTime.Now);
+            UltimaCompra = DateOnly.FromDateTime(DateTime.Now);
+            Situacao = 'A';
+        }
+
+        public Cliente(string data)
+        {
+            Cpf = data.Substring(0, 11);
+            Nome = data.Substring(11, 50);
+
+            DataNascimento = ConverterParaData(data.Substring(61, 8));
+
+            Sexo = char.Parse(data.Substring(69, 1));
+
+            UltimaCompra = ConverterParaData(data.Substring(70, 8));
+
+            DataCadastro = ConverterParaData(data.Substring(78, 8));
+            Situacao = char.Parse(data.Substring(86, 1));
+
+        }
+
+        private DateOnly ConverterParaData(string data)
+        {
+            string dia = data.Substring(0, 2);
+            string mes = data.Substring(2, 2);
+            string ano = data.Substring(4, 4);
+
+            return DateOnly.Parse($"{dia}/{mes}/{ano}");
+        }
+
+
+        public static bool VerificarCpf(string cpf)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string FormatarParaArquivo()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
