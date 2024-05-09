3857cc28fc5475e5fc23a1481982733e0a68c558
Edenilson <edenilsonju10@gmail.com>
2024-05-09 15:40:30 -0300

    criacao estrutura projeti

---
+++ BILTIFUL/Modulo2/MainModulo2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BILTIFUL.Modulo2
+{
+    internal class MainModulo2
+    {
+    }
+}
